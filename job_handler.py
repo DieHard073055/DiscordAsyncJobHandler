b4a218181cee7e9a84bd5e23854a434483ed76d9
Pamela Dayne <pam.dayne@gmail.com>
2023-05-01 16:53:22 +0800

    User `{user.mention}` to properly mention a user

--- job_handler.py
+++ job_handler.py
@@ -16,15 +16,15 @@ class JobQueue:
         self.jobs = queue.Queue()
 
     async def add_job(
-        self, job_type: str, channel_id: int, user_name: str, input: dict[str, str]
+        self, job_type: str, channel_id: int, user: str, input: dict[str, str]
     ) -> None:
         logging.info("Adding a job to the queue")
-        self.jobs.put((job_type, input, channel_id, user_name))
+        self.jobs.put((job_type, input, channel_id, user))
 
     async def run_jobs(self, client: discord.Client, job_function) -> None:
         while True:
             try:
-                job_type, input, channel, user_id = self.jobs.get(block=False)
+                job_type, input, channel, user = self.jobs.get(block=False)
                 logging.info(
                     f"Job [{job_type}] found, executing..."
                 )  # Add this line to see if a job is being executed
@@ -34,7 +34,7 @@ class JobQueue:
 
             try:
                 job_result = await job_selector(
-                    job_type, client, input, channel, user_id
+                    job_type, client, input, channel, user
                 )
             except Exception as e:
                 logging.error(e)
@@ -42,7 +42,7 @@ class JobQueue:
             self.jobs.task_done()
 
 
-async def job_selector(job_type, client, input, channel_id, user_name):
+async def job_selector(job_type, client, input, channel_id, user):
     job_type_map = {
         JOB_IMG: job_img,
         JOB_TXT: job_txt,
@@ -50,19 +50,19 @@ async def job_selector(job_type, client, input, channel_id, user_name):
     logging.info(
         "Executing job selector..."
     )  # Add this line to see if the function is being executed
-    await job_type_map[job_type](client, input, channel_id, user_name)
+    await job_type_map[job_type](client, input, channel_id, user)
     logging.info(f"done executing job")
 
 
 # TODO: find a model that can handle text
-async def job_txt(client, input, channel_id, user_name):
+async def job_txt(client, input, channel_id, user):
     logging.info(
         "Executing txt job function..."
     )  # Add this line to see if the function is being executed
     url = "http://192.168.1.123:5001/predictions"
     headers = {"Content-Type": "application/json"}
     data = {"input": input}
-    result = "lol text model is down right now."
+    result = "lol text model is down right now. ;D"
 
     # async with aiohttp.ClientSession() as session:
     #     async with session.post(
@@ -74,13 +74,13 @@ async def job_txt(client, input, channel_id, user_name):
     #         else:
     #             logging.error(f"Request failed with status code {response.status}")
 
-    coro = client.get_channel(channel_id).send(f"@{user_name}: {result}")
+    coro = client.get_channel(channel_id).send(f"{user.mention} : {result}")
     future = asyncio.run_coroutine_threadsafe(coro, client.loop)
     future.result()
-    logging.info(f"sending response to @{user_name}")
+    logging.info(f"sending response to @{user}")
 
 
-async def job_img(client, input, channel_id, user_name):
+async def job_img(client, input, channel_id, user):
     logging.info(
         "Executing img job function..."
     )  # Add this line to see if the function is being executed
@@ -94,16 +94,18 @@ async def job_img(client, input, channel_id, user_name):
         ) as response:
             if response.status == 200:
                 response_data = await response.json()
-                png_data = base64.b64decode(response_data["output"].split(",")[1])
+                png_data = base64.b64decode(
+                    response_data["output"].split(",")[1])
                 with open("output.png", "wb") as f:
                     f.write(png_data)
             else:
-                logging.error(f"Request failed with status code {response.status}")
+                logging.error(
+                    f"Request failed with status code {response.status}")
                 logging.error(response.text)
 
     coro = client.get_channel(channel_id).send(
-        f"@{user_name} Here's your image!", file=discord.File("output.png")
+        f"@{user} Here's your image!", file=discord.File("output.png")
     )
     future = asyncio.run_coroutine_threadsafe(coro, client.loop)
     future.result()
-    logging.info(f"sending response to @{user_name}")
+    logging.info(f"sending response to @{user}")
