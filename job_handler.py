6ec3d5d998b5e515ad72e0dfc2afd139b7638fb7
Pamela Dayne <pam.dayne@gmail.com>
2023-05-01 22:35:07 +0800

    Add embedded message to print out details of generated image

--- job_handler.py
+++ job_handler.py
@@ -19,12 +19,13 @@ class JobQueue:
         self, job_type: str, channel_id: int, user: str, input: dict[str, str]
     ) -> None:
         logging.info("Added job to queue")
-        self.jobs.put((job_type, input, channel_id, user))
+        self.jobs.put((job_type, channel_id, user, input))
 
     async def run_jobs(self, client: discord.Client, job_function) -> None:
         while True:
             try:
-                job_type, input, channel, user = self.jobs.get(block=False)
+                job_type, channel_id, user, input = self.jobs.get(
+                    block=False)
                 # Add the following line to see if a job is being executed
                 logging.info(f"Job [{job_type}] found, executing...")
             except queue.Empty:
@@ -33,7 +34,7 @@ class JobQueue:
 
             try:
                 job_result = await job_selector(
-                    job_type, client, input, channel, user
+                    job_type, client, input, channel_id, user
                 )
             except Exception as e:
                 logging.error(e)
@@ -59,7 +60,7 @@ async def job_txt(client, input, channel_id, user):
     url = "http://192.168.1.123:5001/predictions"
     headers = {"Content-Type": "application/json"}
     data = {"input": input}
-    result = "lol text model is down right now. :P"
+    result = "lol `/txt` model is down right now. :P"
 
     # async with aiohttp.ClientSession() as session:
     #     async with session.post(
@@ -100,9 +101,36 @@ async def job_img(client, input, channel_id, user):
                     f"Request failed. [Status code: {response.status}]")
                 logging.error(response.text)
 
-    coro = client.get_channel(channel_id).send(
-        f"**{input['prompt']}** - {user.mention}", file=discord.File("output.png")
-    )
-    future = asyncio.run_coroutine_threadsafe(coro, client.loop)
-    future.result()
+    data_embed = discord.Embed()
+    data_embed.add_field(
+        name='prompt', value=input["prompt"], inline=False)
+    data_embed.add_field(
+        name='width', value=input["width"], inline=True)
+    data_embed.add_field(
+        name='height', value=input["height"], inline=True)
+    data_embed.add_field(
+        name="cfg scale", value=input["cfg_scale"], inline=True)
+    data_embed.add_field(
+        name="steps", value=input["steps"], inline=True)
+    data_embed.add_field(
+        name="sampler", value=input["sampler_name"], inline=True)
+    data_embed.add_field(
+        name="seed", value=input["seed"], inline=True)
+    data_embed.add_field(
+        name="negatives", value=input["negative_prompt"], inline=False)
+
+    coro_items = [
+        bot_send_msg(channel_id, client),
+        client.get_channel(channel_id).send(
+            f"{user.mention}, here's your image:", file=discord.File("output.png"), embed=data_embed
+        )
+    ]
+    for coro in coro_items:
+        future = asyncio.run_coroutine_threadsafe(coro, client.loop)
+        future.result()
     logging.info(f"Response sent to {user}")
+
+
+async def bot_send_msg(channel_id: int, client: discord.Client):
+    channel = client.get_channel(channel_id)
+    await channel.send('✅ Done!')
